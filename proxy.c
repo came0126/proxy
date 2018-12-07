f54b0fbe61051f3767722786d12d380608472492
Christian Cameron <christiancameron@cslab05.wheaton.edu>
2018-12-06 20:27:52 -0600

    Fixed proxy setup bug

--- proxy.c
+++ proxy.c
@@ -11,14 +11,16 @@
 #define MAX_OBJECT_SIZE 102400
 #define BACKLOG 25
 
-void parse(char *req, int reg_size);
+void parse(char *req);
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0r\n";
 
 int main(int argc, char **argv) {
+	signal(SIGPIPE, SIG_IGN);
+
 	int cfd;
-	char *hostname, buf[MAXLINE], *port;
+	char buf[MAXLINE], *port;
 	struct sockaddr_in client_addr;
 
 	// Validate port number
@@ -37,7 +39,7 @@ int main(int argc, char **argv) {
 
 	printf("Listening for a request\n");
 	
-   
+
 	// Accept connection
 	socklen_t client_len = sizeof(client_addr);
 	cfd = accept(fd, (struct sockaddr *) &client_addr, &client_len);
@@ -59,26 +61,20 @@ int main(int argc, char **argv) {
 	printf("Received request: %s\n", buf);
 
 	// Write to the client
-	if(write(cfd, "Request received", 16) < 0) {
+	if(rio_writen(cfd, "Request received", 16) < 0) {
 		perror("Error writing to client\n");
 		exit(2);
 	}
 
-	parse(buf, size);
+	parse(buf);
 
-	hostname = NULL;
-	// Open connection to a server
-	cfd = open_clientfd(hostname, port);
-	
-    printf("%s\n", user_agent_hdr);
-
-    close(cfd);
-    close(fd);
-    return 0;
+	close(cfd);
+	close(fd);
+	return 0;
 }
 
-/** Parse the client's request, supporting only GET commands **/
-void parse(char *req, int reg_size) {
+/** Parse and send the client's request, supporting only GET commands **/
+void parse(char *req) {
 	char uri[MAXLINE], req_type[MAXLINE], http[MAXLINE], path[MAXLINE], leftover[MAXLINE], host[MAXLINE], nohttp[MAXLINE], port[5];
 
 	// Parse the request
@@ -91,14 +87,30 @@ void parse(char *req, int reg_size) {
 		port[1] = '0';
 	}
 
-	// printf("uri: %s\n", uri);
-	// printf("http: %s\n", http);
-	// printf("path: %s\n", path);
-	// printf("port: %s\n", port);
-	// printf("req_type: %s\n", req_type);
-
 	if(strcmp(req_type, "GET") == 0) {
-		
+
+		// Open the server connection
+		int cfd = open_clientfd(host, port);
+		if(cfd < 0) {
+			perror("Error with open_clientfd\n");
+			exit(2);
+		}
+
+		char header[MAXLINE];
+		sprintf(header, "GET %s HTTP/1.0\r\nHost: %s\r\n%s\r\nConnection: close\r\nProxy-Connection: close\r\n\r\n", path, host, user_agent_hdr);
+
+
+		if(rio_writen(cfd, "test", 4) < 0) {
+			perror("Error with send\n");
+			exit(2);
+		}
+
+		// if(send(cfd, header, strlen(header), 0) < 0) {
+		// 	perror("Error with send\n");
+		// 	exit(2);
+		// }
+
+
 	}
 
 	return;
